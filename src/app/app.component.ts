dddc85ec0cb643875d2c495071266a2acab03545
HeidiHyny <heidi.hynynen@student.laurea.fi>
2018-11-14 16:01:14 +0200

    First commit
    Comes with basic functionalities (sidemenu and Firebase Login)

---
+++ src/app/app.component.ts
@@ -0,0 +1,64 @@
+import { Component, ViewChild } from '@angular/core';
+import { Nav, Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  @ViewChild(Nav) nav: Nav;
+
+  rootPage: any = LoginPage;
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, private angularFireAuth: AngularFireAuth) {
+    this.initializeApp();
+
+    // used for an example of ngFor and navigation
+    this.pages = [
+      { title: 'Home', component: HomePage },
+      { title: 'List', component: ListPage },
+      { title: 'Login', component: LoginPage },
+      { title: 'Register', component: RegisterPage}
+    ];
+
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      this.statusBar.styleDefault();
+      this.splashScreen.hide();
+
+      //If authenticated user make rootpage homepage otherwise direct to loginpage
+      this.angularFireAuth.auth.onAuthStateChanged(function(user)
+     {
+       if (user) {
+       this.rootPage = HomePage;
+     }
+     else {
+     this.rootPage = LoginPage;
+
+     }
+   });
+    });
+  }
+//Push to the rootpage on start
+ngOnInit() {
+  this.nav.push(this.rootPage);
+ }
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+import { RegisterPage } from '../pages/register/register';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+//Importing all the Firebase-components
+import { AngularFireModule } from 'angularfire2/';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuth } from 'angularfire2/auth';
+
+//Importing the FIREBASE_CONFIG
+import { FIREBASE_CONFIG } from '../providers/firebase-credentials/firebase-credentials';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,
+    LoginPage,
+    RegisterPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    //Importing Firebase-components:
+   AngularFireModule.initializeApp(FIREBASE_CONFIG),
+   AngularFireDatabaseModule,
+   AngularFireAuthModule,
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,
+    LoginPage,
+    RegisterPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+     //Importing Firebase-AngularFirebaseAuth
+   AngularFireAuth,
+  ]
+})
+export class AppModule {}
